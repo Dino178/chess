fa2804e45e2e8aef63baf408e17823d9732a24a9
Dino178 <landon.z.low@gmail.com>
2026-09-08 20:51:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -857,7 +857,7 @@ let placementStep = 0;
 const PIECE_VALUES = { 'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9, 'k': 0 };
 
 const config = {
-    draggable: true,
+    draggable: false,
     position: 'start',
     onDragStart: onDragStart,
     onDrop: onDrop,
